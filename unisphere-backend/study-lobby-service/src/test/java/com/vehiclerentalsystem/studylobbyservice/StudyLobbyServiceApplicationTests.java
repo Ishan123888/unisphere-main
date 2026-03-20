97d16e461850ca6aa8fa47b25e66067fe0e8f30c
Kavishka Wijewardhana <kaviwijewardhana@gmail.com>
2026-03-20 14:00:16 +0530

    initial commit

---
+++ unisphere-backend/study-lobby-service/src/test/java/com/vehiclerentalsystem/studylobbyservice/StudyLobbyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiclerentalsystem.studylobbyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLobbyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
